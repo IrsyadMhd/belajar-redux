3571705d41aabf27d9d2914fc83d1c45bbd223a1
Toko Yaakhi <mhd.irsyad@gmail.com>
2021-10-19 15:54:08 +0700

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({});
+import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
+
+// const initialItemCounter = { counter: 0, isShow: false };
+
+// const itemCounterSlice = createSlice({
+//   name: "item",
+//   initialState: initialItemCounter,
+//   reducers: {
+//     addItem(state) {
+//       state.counter++;
+//     },
+//     decItem(state) {
+//       state.counter--;
+//     },
+//     toggle(state) {
+//       state.isShow = !state.isShow;
+//     },
+//   },
+// });
+
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
+});
+
+// export const itemActions = itemCounterSlice.actions;
 
 export default store;
